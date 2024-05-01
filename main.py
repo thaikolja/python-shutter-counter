0b74a1da6a163a23bf6f133e4d166364590a6154
Kolja Nolte <kolja.nolte@gmail.com>
2024-05-02 03:08:21 +0700

    Fixed no output when parameter `str` provided
    Signed-off-by: Kolja Nolte <kolja.nolte@gmail.com>

--- main.py
+++ main.py
@@ -66,29 +66,29 @@ class ShutterCounter:
                 camera_model = str(tags['Image Model'])
 
                 if camera_model not in self.supported_models:
-                    error(f"Your camera model {camera_model} is currently not supported.")
+                    error(f" Your camera model {camera_model} is currently not supported.")
                     exit(1)
 
                 if self.shutter_tag in tags:
                     shutter_count_str = str(tags[self.shutter_tag])
 
                     try:
+                        output = str(f"Shutter count: {shutter_count_str}")
+
                         if len(argv) > 2 and argv[2] == 'int':
                             output = int(shutter_count_str)
-                        else:
-                            output = str(f"Shutter count: {shutter_count_str}")
 
                         return output
 
                     except ValueError:
-                        error(f"Could not convert shutter count to ${output_type}: {shutter_count_str}")
+                        error(f" Could not convert shutter count to ${output_type}: {shutter_count_str}")
                         exit(1)
                 else:
-                    warning(f"The shutter count could not be found in the EXIF data.")
+                    warning(f" The shutter count could not be found in the EXIF data.")
                     exit(1)
 
         except (FileNotFoundError, IOError):
-            error(f"Could not find or open the image file: {self.image_path}")
+            error(f" Could not find or open the image file: {self.image_path}")
             exit(1)
 
 
@@ -104,7 +104,6 @@ if __name__ == "__main__":
     # Check if the result type is provided as an argument
     if len(argv) > 2 and argv[2] == 'str':
         result_type = argv[2]
-        exit(1)
 
     # Check if help is requested
     if argv[1] == '-h' or argv[1] == '--help':
@@ -113,7 +112,7 @@ if __name__ == "__main__":
 
     # Check if the Python version is 3 or above
     if version_info[0] < 3:
-        error("This script requires Python 3.")
+        error(" This script requires Python 3.")
         exit(1)
 
     # Create a ShutterCounter object
